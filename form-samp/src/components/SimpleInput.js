239c45aa7e2d556a7444f8f9a6261d53aec361c9
Abhijith S <abheaks.007@gmail.com>
2022-09-22 11:45:03 +0530

    food order app

--- form-samp/src/components/SimpleInput.js
+++ form-samp/src/components/SimpleInput.js
@@ -1,12 +1,45 @@
+import React, { useState } from "react";
+
 const SimpleInput = (props) => {
+  const [name, setName] = useState("");
+  const [enteredNameIsvalid, setEnterednameIsValid] = useState(true);
+  // const nameInputRef = useRef();
+
+  const inputHandler = (event) => {
+    setName(event.target.value);
+  };
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    if (name.trim() === "") {
+      setEnterednameIsValid(false);
+      return;
+    }
+    setEnterednameIsValid(true);
+
+    console.log(name);
+    // const enteredValue = nameInputRef.current.value;
+    // console.log("Ref:", enteredValue);
+    setName("");
+  };
+
   return (
     <form>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+      <div className="form-control">
+        <label htmlFor="name">Your Name</label>
+        <input
+          // ref={nameInputRef}
+          type="text"
+          id="name"
+          value={name}
+          onChange={inputHandler}
+        />
+        {!enteredNameIsvalid && (
+          <p className="error-text">Name must not be empty</p>
+        )}
       </div>
       <div className="form-actions">
-        <button>Submit</button>
+        <button onClick={submitHandler}>Submit</button>
       </div>
     </form>
   );
